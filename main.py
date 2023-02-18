ebaf5e1df9c343a058dbf8d6f9542404114ad254
rusinek-wojciech <59565620+rusinek-wojciech@users.noreply.github.com>
2023-02-18 15:24:12 +0100

    probably issue with wrong f format in obj is fixed but texture coordinate is still missing

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import glfw
 from OpenGL.GL import *
+import numpy as np
 import pyrr
 from shader import createShader
-from utils import load_texture
-from ObjLoader import ObjLoader
-
+from utils import load_texture, load_model
 from movement import Movement
 from generator import generate
 
+# -----------------------------------------------------------------
+
 WIDTH, HEIGHT = 1280, 720
-IS_RANDOM_TERRAIN = True
-terrain_path = "meshes/generated_terrain.obj" if IS_RANDOM_TERRAIN else "meshes/bieszczady_10_2.obj"
+IS_RANDOM_TERRAIN = False
+terrain_path = "meshes/generated_terrain.obj" if IS_RANDOM_TERRAIN else "meshes/untitled.obj"
 texture_path = "meshes/generated_terrain.png" if IS_RANDOM_TERRAIN else "meshes/mapa_kolorowa.jpg"
 
 # terrain_path = "meshes/generated_terrain.obj" if IS_RANDOM_TERRAIN else "meshes/testowe_ze_zdj.obj"
@@ -18,7 +19,9 @@ texture_path = "meshes/generated_terrain.png" if IS_RANDOM_TERRAIN else "meshes/
 
 movement = Movement(WIDTH, HEIGHT)
 
-def window_resize_callback(window, width, height):
+# -----------------------------------------------------------------
+
+def window_resize(window, width=WIDTH, height=HEIGHT):
     glViewport(0, 0, width, height)
     projection = pyrr.matrix44.create_perspective_projection_matrix(45, width / height, 0.1, 100)
     glUniformMatrix4fv(proj_loc, 1, GL_FALSE, projection)
@@ -33,49 +36,61 @@ if not window:
     glfw.terminate()
     raise Exception("glfw window can not be created!")
 
-glfw.set_window_size_callback(window, window_resize_callback)
+glfw.set_window_size_callback(window, window_resize)
 glfw.set_cursor_pos_callback(window, movement.mouse_callback)
 glfw.set_key_callback(window, movement.keyboard_callback)
 glfw.set_input_mode(window, glfw.CURSOR, glfw.CURSOR_DISABLED)
 glfw.make_context_current(window)
 
+# -----------------------------------------------------------------
+
+shader = createShader()
+glUseProgram(shader)
+
+model_loc = glGetUniformLocation(shader, "model")
+proj_loc = glGetUniformLocation(shader, "projection")
+view_loc = glGetUniformLocation(shader, "view")
+
+window_resize(None)
+floor_pos = pyrr.matrix44.create_from_translation(pyrr.Vector3([0, 0, 0]))
+glUniformMatrix4fv(model_loc, 1, GL_FALSE, floor_pos)
+
+# -----------------------------------------------------------------
+
 if IS_RANDOM_TERRAIN:
     generate()
 
-terrain_indices, terrain_buffer = ObjLoader.load_model(terrain_path)
-shader = createShader()
+terrain_indices, terrain_buffer = load_model(terrain_path)
+buffer = np.array(terrain_buffer, dtype='float32')
 
 VAO = glGenVertexArrays(2)
 VBO = glGenBuffers(2)
 
 glBindVertexArray(VAO[0])
 glBindBuffer(GL_ARRAY_BUFFER, VBO[0])
-glBufferData(GL_ARRAY_BUFFER, terrain_buffer.nbytes, terrain_buffer, GL_STATIC_DRAW)
+glBufferData(GL_ARRAY_BUFFER, buffer.nbytes, buffer, GL_STATIC_DRAW)
 
+glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 32, ctypes.c_void_p(0))
 glEnableVertexAttribArray(0)
-glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, terrain_buffer.itemsize * 8, ctypes.c_void_p(0))
+glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 32, ctypes.c_void_p(12))
 glEnableVertexAttribArray(1)
-glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, terrain_buffer.itemsize * 8, ctypes.c_void_p(12))
-glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, terrain_buffer.itemsize * 8, ctypes.c_void_p(20))
+glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 32, ctypes.c_void_p(20))
 glEnableVertexAttribArray(2)
 
+image, image_bytes = load_texture(texture_path)
 textures = glGenTextures(2)
-load_texture(texture_path, textures[0])
 
-glUseProgram(shader)
 glClearColor(0.53, 0.8, 0.92, 1)
 glEnable(GL_DEPTH_TEST)
 glEnable(GL_BLEND)
 glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
 
-projection = pyrr.matrix44.create_perspective_projection_matrix(45, WIDTH / HEIGHT, 0.1, 100)
-floor_pos = pyrr.matrix44.create_from_translation(pyrr.Vector3([0, 0, 0]))
-
-model_loc = glGetUniformLocation(shader, "model")
-proj_loc = glGetUniformLocation(shader, "projection")
-view_loc = glGetUniformLocation(shader, "view")
-
-glUniformMatrix4fv(proj_loc, 1, GL_FALSE, projection)
+glBindTexture(GL_TEXTURE_2D, textures[0])
+glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
+glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
+glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image.width, image.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image_bytes)
 
 while not glfw.window_should_close(window):
     glfw.poll_events()
@@ -85,7 +100,6 @@ while not glfw.window_should_close(window):
     glUniformMatrix4fv(view_loc, 1, GL_FALSE, movement.get_view())
 
     glBindVertexArray(VAO[0])
-    glUniformMatrix4fv(model_loc, 1, GL_FALSE, floor_pos)
     glDrawArrays(GL_TRIANGLES, 0, len(terrain_indices))
 
     glfw.swap_buffers(window)
--- utils.py
+++ utils.py
@@ -1,16 +1,86 @@
 from OpenGL.GL import *
 from PIL import Image
 
+def load_texture(path):
+    image = Image.open(path).transpose(Image.FLIP_TOP_BOTTOM)
+    image_bytes = image.convert("RGBA").tobytes()
+    return image, image_bytes
 
-def load_texture(path, texture):
-    glBindTexture(GL_TEXTURE_2D, texture)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
-    image = Image.open(path)
-    image = image.transpose(Image.FLIP_TOP_BOTTOM)
-    img_data = image.convert("RGBA").tobytes()
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image.width, image.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, img_data)
-    return texture
 
+def load_model(file):
+    buffer = []
+    vertices = []
+    textures = []
+    normals = []
+    all_indices = []
+    indices = []
+
+
+    is_first = True
+    f_format = -1
+
+    with open(file, 'r') as f:
+        line = f.readline()
+        while line:
+            values = line.split()
+            if values[0] == 'v':
+                for value in values[1:]:
+                    vertices.append(float(value))
+
+            elif values[0] == 'vt':
+                for value in values[1:]:
+                    textures.append(float(value))
+
+            elif values[0] == 'vn':
+                for value in values[1:]:
+                    normals.append(float(value))
+
+            elif values[0] == 'f':
+                for value in values[1:]:
+                    vals = value.split('/')
+
+                    if is_first:
+                        f_format = len(vals)
+                        is_first = False
+
+                    for v in vals:
+                        all_indices.append(int(v) - 1)
+                    indices.append(int(vals[0]) - 1)
+
+            line = f.readline()
+
+
+    if f_format == 1:
+        for i, ind in enumerate(all_indices):
+            start = ind * 3
+            end = start + 3
+            buffer.extend(vertices[start:end])
+            buffer.extend([0.5, 0.5])
+            buffer.extend([0.0, 0.0, 0.0])
+    elif f_format == 2:
+        for i, ind in enumerate(all_indices):
+            if i % 2 == 0:
+                start = ind * 3
+                end = start + 3
+                buffer.extend(vertices[start:end])
+            elif i % 2 == 1:
+                start = ind * 2
+                end = start + 2
+                buffer.extend(textures[start:end])
+                buffer.extend([0.0, 0.0, 0.0])
+    elif f_format == 3:
+        for i, ind in enumerate(all_indices):
+            if i % 3 == 0:
+                start = ind * 3
+                end = start + 3
+                buffer.extend(vertices[start:end])
+            elif i % 3 == 1:
+                start = ind * 2
+                end = start + 2
+                buffer.extend(textures[start:end])
+            elif i % 3 == 2:
+                start = ind * 3
+                end = start + 3
+                buffer.extend(normals[start:end])
+
+    return indices, buffer
